4ea40e0a331b3096e72dd1b2f589484a04f153e5
uthsa <uthsavi29@gmail.com>
2020-11-10 02:41:45 +0530

    Added:To Flip A Coin To Show Number Of Times Head Won And Tail Won

--- flipCoinSimulator.sh
+++ flipCoinSimulator.sh
@@ -12,3 +12,27 @@ else
         echo "Tail Is Winner"
 fi
 
+echo "To Loop Through Multile times To Flip A Coin To Display No. Of Times Head And Tail Has Won"
+
+read -p "Enter the number of coin flip -" coinFlip
+
+headCount=0;
+tailCOunt=0;
+
+for((index=1; index<=$coinFlip; index++))
+do
+	if [ $((RANDOM%2)) -eq 1 ]
+	then
+		echo "Head is the winner"
+		((headCount++))
+
+	else
+		echo "Tail is the winner"
+		((tailCount++))
+
+	fi
+done
+
+echo "Head Won" $headCount "times"
+echo "Tail Won" $tailCount "times"
+
